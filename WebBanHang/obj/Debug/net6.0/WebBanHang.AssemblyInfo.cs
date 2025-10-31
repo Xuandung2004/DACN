f8ee30c9d80e8c63fa6dc38d067598fec274172d
dungcavang <xuandung026@gmail.comm>
2025-10-31 15:27:37 +0700

    Làm sạch repo sau khi cập nhật .gitignore

--- WebBanHang/obj/Debug/net6.0/WebBanHang.AssemblyInfo.cs
+++ WebBanHang/obj/Debug/net6.0/WebBanHang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c7e47cd7be340d2b6dae6575e5e06babc23115")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
