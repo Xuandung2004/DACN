7530707e5f0243a6394f32d25380e28efb8bb86e
Hieutinhtee <hieuthaiminh05@gmail.com>
2025-11-04 21:29:24 +0700

    Update connection string and ignore context file
    Changed the database password in CsdlFinal1Context.cs connection string. Added CsdlFinal1Context.cs to .gitignore and removed .vs temporary files from the repository.

--- DACN_Web_API/Models/CsdlFinal1Context.cs
+++ DACN_Web_API/Models/CsdlFinal1Context.cs
@@ -44,7 +44,7 @@ public partial class CsdlFinal1Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=csdl_final1;user=root;password=123456", Microsoft.EntityFrameworkCore.ServerVersion.Parse("9.4.0-mysql"));
+        => optionsBuilder.UseMySql("server=localhost;database=csdl_final1;user=root;password=abc123@", Microsoft.EntityFrameworkCore.ServerVersion.Parse("9.4.0-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
