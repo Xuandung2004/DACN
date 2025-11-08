76427d0c3a8703995a9d03e13a71105623f40261
Hieutinhtee <hieuthaiminh05@gmail.com>
2025-11-08 18:34:47 +0700

    cập nhật xem ds sản phẩm ở trang chủ

---
+++ DACN_Web_API/Controllers/DanhSachSanPhamController.cs
@@ -0,0 +1,61 @@
+﻿using DACN_Web_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DACN_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DanhSachSanPhamController : ControllerBase
+    {
+
+        private CsdlFinal1Context _context = new CsdlFinal1Context();
+        //  GET: api/DanhSachSanPham
+        [HttpGet]
+        public async Task<IActionResult> GetSanPhams()
+        {
+            var sanPhams = await _context.Sanphams
+                .Include(sp => sp.Anhs)
+                .Select(sp => new
+                {
+                    sp.Id,
+                    sp.TenSp,
+                    sp.Gia,
+                    Anh = sp.Anhs.Select(a => a.Url).ToList()
+                    //KichThuoc = sp.Kichthuocs.Select(k => k.SoLieu).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(sanPhams);
+        }
+
+        //  GET: api/DanhSachSanPham/5 (chi tiết sản phẩm)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSanPham(int id)
+        {
+            var sp = await _context.Sanphams
+                .Include(s => s.DanhMuc)
+                .Include(s => s.Kichthuocs)
+                .Include(s => s.Anhs)
+                .FirstOrDefaultAsync(s => s.Id == id);
+
+            if (sp == null)
+                return NotFound();
+
+            var sanPhamChiTiet = new
+            {
+                sp.Id,
+                sp.TenSp,
+                sp.Gia,
+                sp.MoTa,
+                DanhMuc = sp.DanhMuc?.TenDm,
+                Anh = sp.Anhs.Select(a => a.Url).ToList(),
+                KichThuoc = sp.Kichthuocs.Select(a => a.SoLieu).ToList()
+            };
+
+            return Ok(sanPhamChiTiet);
+        }
+    }
+}
