2ce5200537867aeb8a87527f405719be5aa07134
Githubopero <truongvankiet04@gmail.com>
2025-11-08 17:30:42 +0700

    xoa anh
    update

--- DACN_Web_API/Controllers/SanPhamController.cs
+++ DACN_Web_API/Controllers/SanPhamController.cs
@@ -45,32 +45,7 @@ namespace DACN_Web_API.Controllers
             }
         }
 
-        //// PUT: api/SanPham/5
-        //[HttpPut("{id}")]
-        //public IActionResult Update(int id, [FromBody] Sanpham input)
-        //{
-        //    if (input == null) return BadRequest("Invalid product payload.");
-        //    if (id != input.Id) return BadRequest("Id in URL and payload do not match.");
-
-        //    var sp = db.Sanphams.Find(id);
-        //    if (sp == null) return NotFound();
-
-        //    // update allowed fields
-        //    sp.TenSp = input.TenSp;
-        //    sp.MoTa = input.MoTa;
-        //    sp.Gia = input.Gia;
-        //    sp.DanhMucId = input.DanhMucId;
-        //    sp.Slug = input.Slug;
-        //    sp.TonKho = input.TonKho;
-        //    sp.AnhId = input.AnhId;
-        //    sp.KichThuocId = input.KichThuocId;
-        //    sp.CapNhat = DateTime.Now;
-
-        //    db.SaveChanges();
-
-        //    // return 204 No Content for a successful update
-        //    return NoContent();
-        //}
+      
 
         // DELETE: api/SanPham/5
         [HttpDelete("{id}")]
