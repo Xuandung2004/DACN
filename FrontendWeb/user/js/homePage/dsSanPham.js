a139b894b4a3de45a68e09fc33370f70611e8daa
Hieutinhtee <hieuthaiminh05@gmail.com>
2025-12-02 23:30:28 +0700

    Update admin UI and localization, improve user management
    Refactored admin dashboard layout, removed unused sidebar items, and updated navigation links for consistency. Improved localization by translating user menu items to Vietnamese. Enhanced user management modal logic in JS, fixing form reset and visibility issues. Cleaned up markup and removed redundant collapse menus in admin pages.

--- FrontendWeb/user/js/homePage/dsSanPham.js
+++ FrontendWeb/user/js/homePage/dsSanPham.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         <div  class="block2-pic hov-img0">
                             <img class="img-product" src="/FrontendWeb/${imgSrc}" alt="${sp.tenSp}" >
                             <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" data-id="${sp.id}">
-                                Quick View
+                                Xem nhanh
                             </a>
                         </div>
                         <div class="block2-txt flex-w flex-t p-t-14">
